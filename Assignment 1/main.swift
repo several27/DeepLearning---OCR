d4fb58f290bbdb5ace5ccacdab8271e8c033f8aa
Maciej Szpakowski <maciek.szpak27@gmail.com>
2016-01-30 11:27:39 +0000

    Initial Commit

---
+++ Assignment 1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment 1
+//
+//  Created by Maciej Szpakowski on 30/01/2016.
+//  Copyright © 2016 Maciej Szpakowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
